3adcc16218b15145802df776c6d01347596b774b
Murilo Feba <murilo.coelho1006@gmail.com>
2025-11-02 22:53:50 -0300

    Adicionar CORS para produção - Render.com

--- server.js
+++ server.js
@@ -19,7 +19,14 @@ const app = express();
 // ======================
 
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'http://10.0.2.2:3000',
+    'https://taskflow-api-055k.onrender.com' // sua URL do Render
+  ],
+  credentials: true
+}));
 app.use(json());
 app.use(express.json());
 
